0de7a1c72034de7e79ea6d2d56a1e4b2017ea787
srini1603 <srininavy3436@gmail.com>
2022-03-26 12:55:27 +0530

    icons is added to REST api tester , font had been changed ,JSON tab is changed to RESPONSE tab where the Response is displayed

--- js/request.js
+++ js/request.js
@@ -4,6 +4,26 @@ const requestHeadersContainer = document.querySelector("[data-request-headers]")
 const keyValueTemplate = document.querySelector("[data-key-value-template]")
 const responseHeadersContainer = document.querySelector("[data-response-headers]")
 
+
+const createKeyValuePair = () => {
+  const element = keyValueTemplate.content.cloneNode(true)
+  element.querySelector("[data-remove-btn]").addEventListener("click", e => {
+    e.target.closest("[data-key-value-pair]").remove()
+  })
+  return element
+}
+
+const keyValuePairsToObjects = (container) => {
+  const pairs = container.querySelectorAll("[data-key-value-pair]")
+  return [...pairs].reduce((data, pair) => {
+    const key = pair.querySelector("[data-key]").value
+    const value = pair.querySelector("[data-value]").value
+
+    if (key === "") return data
+    return { ...data, [key]: value }
+  }, {})
+}
+
 document
   .querySelector("[data-add-query-param-btn]")
   .addEventListener("click", () => {
@@ -20,128 +40,50 @@ queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
 
-function createKeyValuePair() {
-  const element = keyValueTemplate.content.cloneNode(true)
-  element.querySelector("[data-remove-btn]").addEventListener("click", e => {
-    e.target.closest("[data-key-value-pair]").remove()
-  })
-  return element
-}
+// function to validate the url
+const validate = (url) =>{ 
+  const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol.val()
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 
-function keyValuePairsToObjects(container) {
-  const pairs = container.querySelectorAll("[data-key-value-pair]")
-  return [...pairs].reduce((data, pair) => {
-    const key = pair.querySelector("[data-key]").value
-    const value = pair.querySelector("[data-value]").value
+  if(url == '') return false; // if url is empty
 
-    if (key === "") return data
-    return { ...data, [key]: value }
-  }, {})
-}
+  else if(!pattern.test(url)) return false;
 
+  else return true;
 
-
-// copied from the video
-// using axios part
-
-// axios.interceptors.request.use(request => {
-//     request.customData = request.customData || {}
-//     request.customData.startTime = new Date().getTime()
-//     return request
-//   })
-  
-//   function updateEndTime(response) {
-//     response.customData = response.customData || {}
-//     response.customData.time =
-//       new Date().getTime() - response.config.customData.startTime
-//     return response
-//   }
-  
-//   axios.interceptors.response.use(updateEndTime, e => {
-//     return Promise.reject(updateEndTime(e.response))
-//   })
-  
-//   const { requestEditor, updateResponseEditor } = setupEditors()
-//   form.addEventListener("submit", e => {
-//     e.preventDefault()
-  
-//     let data
-//     try {
-//       data = JSON.parse(requestEditor.state.doc.toString() || null)
-//     } catch (e) {
-//       alert("JSON data is malformed")
-//       return
-//     }
-  
-//     axios({
-//       url: document.querySelector("[data-url]").value,
-//       method: document.querySelector("[data-method]").value,
-//       params: keyValuePairsToObjects(queryParamsContainer),
-//       headers: keyValuePairsToObjects(requestHeadersContainer),
-//       data,
-//     })
-//       .catch(e => e)
-//       .then(response => {
-//         document
-//           .querySelector("[data-response-section]")
-//           .classList.remove("d-none")
-//         updateResponseDetails(response)
-//         updateResponseEditor(response.data)
-//         updateResponseHeaders(response.headers)
-//         console.log(response)
-//       })
-//   })
-  
-//   function updateResponseDetails(response) {
-//     document.querySelector("[data-status]").textContent = response.status
-//     document.querySelector("[data-time]").textContent = response.customData.time
-//     document.querySelector("[data-size]").textContent = prettyBytes(
-//       JSON.stringify(response.data).length +
-//         JSON.stringify(response.headers).length
-//     )
-//   }
-  
-//   function updateResponseHeaders(headers) {
-//     responseHeadersContainer.innerHTML = ""
-//     Object.entries(headers).forEach(([key, value]) => {
-//       const keyElement = document.createElement("div")
-//       keyElement.textContent = key
-//       responseHeadersContainer.append(keyElement)
-//       const valueElement = document.createElement("div")
-//       valueElement.textContent = value
-//       responseHeadersContainer.append(valueElement)
-//     })
-//   }
-
-
-// params
-// listtdown
-
+};
 
 const sendDetails = () => {
   const url = $('#url').val();
-  const type = document.querySelector("[data-method]").value;
+  const type = $("[data-method]").val();
   const header = keyValuePairsToObjects(requestHeadersContainer);
   const params = keyValuePairsToObjects(queryParamsContainer);
-  // let jsonData;
-  // try {
-  //   jsonData = JSON.parse( || null)
-  // } catch (e) {
-  //   alert("JSON data is malformed")
-  //   return
-  // }
+
+  if(!validate(url)) return alert("enter a valid url");
 
   $.ajax({
     type: type ,
     data: params,
-    timeout: 60000,
+    timeout: 60000, 
     url: url,
     params: params,
     headers: header,
-}).then((response) => console.log(response,header,params));
+})
+  .catch(e => e)
+  .then(response => {
+  let parsedata = response;
+  let DatainprettyFormat = JSON.stringify(parsedata,undefined,4);
+  document.getElementById('prettyJSONFormat').value = DatainprettyFormat;
+})
 };
 
-$(document).on('click', '#sendDetails', () => {
+$(document).on( 'click','#sendDetails', () => {
+  $('#sendDetails').click(function(event){
+    event.preventDefault();
+  });
   sendDetails();
 });
-
